2ff850763104ffd3e3c25f4e4dae0c6b2a02a4ed
JinheonBaek <Jinheon.Baek@outlook.kr>
2017-10-20 20:53:55 +0900

    5. / Combine all models in main.py

--- 5. Temp Combine - 10.20/main.py
+++ 5. Temp Combine - 10.20/main.py
@@ -7,7 +7,7 @@ import scenedetect.detectors
 import scenedetect.manager
 from blooddetect.blood import BloodColorDetector
 from sensationalismdetect import vision
-from movementdescriptor import extract_movementDescriptor
+from movementdescriptor import movementDescriptor
 
 def scene_detect(filename = None):
 
@@ -63,29 +63,46 @@ def sensationalism_detect(movie_name = None, images = None, scene_num = None, cu
         wr.writerow([scene_num, i, result])
 
     f.close()
-    
+
+def motiondescriptor(movie_name = None, images = None, scene_num = None, frame_num = None):
+    data_dir_path = "./movementdescriptor_data/" + movie_name
+    if not os.path.exists(str(data_dir_path)):
+        os.makedirs(str(data_dir_path))
+    output_name = "Movie-%sScene-%04d.csv" % (movie_name, scene_num)
+    data_path_prefix = data_dir_path + '\\' + output_name
+
+    f = open(data_path_prefix, 'w', encoding='utf-8', newline='')
+    wr = csv.writer(f)
+
+    MBM_mean = movementDescriptor.MBM(images, frame_num)
+    wr.writerow(MBM_mean)
+
+    f.close()
 
 def main():
     cap = cv2.VideoCapture()
-    
     movie_name = "my_video.mp4"
-    #scene_list = scene_detect(filename = filename)
-    scene_list = [84, 140, 288]
+    scene_list = scene_detect(filename = movie_name)
     scene_list.insert(0, 0)
 
     cap.open("./video/" + movie_name)
+    frame_num = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     
     for scene_num in range(len(scene_list) - 1):
         images = []
         cut_num = scene_list[scene_num + 1] - scene_list[scene_num]
-        
+        print(movie_name, "percent: " + scene_list[scene_num] / frame_num)
+
         for j in range(cut_num):
             (ret_val, im_cap) = cap.read()
             images.append(im_cap)
 
-        blood_detect(movie_name = movie_name, images = images, detector = BloodColorDetector(), scene_num = scene_num, cut_num = cut_num)
-        sensationalism_detect(movie_name = movie_name, images = images, scene_num = scene_num, cut_num = cut_num)
-        #MBM()
+        print(movie_name, "Scene: " + scene_num, "blood_detect")
+        #blood_detect(movie_name = movie_name, images = images, detector = BloodColorDetector(), scene_num = scene_num, cut_num = cut_num)
+        print(movie_name, "Scene: " + scene_num, "sensationalism_detect")
+        #sensationalism_detect(movie_name = movie_name, images = images, scene_num = scene_num, cut_num = cut_num)
+        print(movie_name, "SceneL " + scene_num, "motiondescriptor_detect")
+        motiondescriptor(movie_name = movie_name, images = images, scene_num = scene_num, frame_num = cut_num)
         break
-
+    
 main()
--- 5. Temp Combine - 10.20/movementdescriptor/movementDescriptor.py
+++ 5. Temp Combine - 10.20/movementdescriptor/movementDescriptor.py
@@ -4,15 +4,10 @@ import math
 from PIL import Image
 import numpy as np
 
-def MBM(path = None, filename = None):
-
-    _path = path + filename
-    cap = cv2.VideoCapture()
-    cap.open(_path)
+def MBM(images = None, frame_num = None):
 
     set_video_width = 1280
     set_video_height = 720
-    frame_num = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
     # number of features = sub_row * sub_col
     sub_row, sub_col = int(set_video_height/15), int(set_video_width/15)
@@ -21,11 +16,9 @@ def MBM(path = None, filename = None):
 
     # Load all frames of video & convert frame to extract flows
     for i in range(frame_num):
-        (ret_val, im_cap) = cap.read()
-        
         # frame reshape
-        frame = np.array(Image.fromarray(im_cap).resize((set_video_width, set_video_height)))
-        frames[i] = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = np.array(Image.fromarray(images[i]).resize((set_video_width, set_video_height)))
+        images[i] = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Extract flows from the two frames
     flows = np.zeros((frame_num-1, set_video_height, set_video_width, 2))
@@ -67,13 +60,7 @@ def MBM(path = None, filename = None):
 
     # Mean
     MBM_mean = MBM.mean(axis=2)
-
-    f = open('HockeyTraining.csv', 'w', encoding='utf-8', newline='')
-    wr = csv.writer(f)
-    li = list(MBM_mean.reshape(MBM_mean.shape[0] * MBM_mean.shape[1]))
-    wr.writerow(li)
-    f.close()
-    
-    cap.release()
+    MBM_mean = MBM_mean.reshape(MBM_mean.shape[0] * MBM_mean.shape[1])
+    return MBM_mean
 
 #MBM(path = "../video/", filename = "my_video.mp4")
